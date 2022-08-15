a5581e63114408ba94673eece63b0b301c6efaec
Martin Lovell <martin.lovell@yale.edu>
2022-08-15 13:13:51 -0400

    Allow adding ranges - Adds button to add new ranges - Allow multi select on tree - Use custom EditText so that double click is needed

---
+++ src/components/EditableText.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+class EditableTextProps {
+  onSave!: ((value: string) => void);
+  defaultValue!: string;
+}
+
+function EditableText({ onSave, defaultValue }: EditableTextProps) {
+
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState(defaultValue);
+
+  if (editing) {
+    return <input className='edit-text' value={value} 
+      onChange={(e) => setValue(e.target.value)}  // using input instead of antd Input since onBlur doesn't work on Input.
+      onBlur={() => {
+        console.log("ONBLUR");
+        setEditing(false);
+        onSave(value);
+      }} onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          setEditing(false);
+          onSave(value);
+          e.preventDefault();
+          e.stopPropagation();
+        } else if (e.key === 'Escape') {
+          setEditing(false);
+          setValue(defaultValue);
+          e.preventDefault();
+          e.stopPropagation();
+        }
+      }} />
+  } else {
+    return <span onDoubleClick={(e) => { setEditing(true) }}>{value}</span>
+  }
+
+}
+
+export default EditableText;
--- src/components/TopHeader.tsx
+++ src/components/TopHeader.tsx
@@ -3,14 +3,19 @@ import React from 'react';
 const { Header } = Layout;
 
 class TopHeaderProps {
+  addRangeEnabled: boolean = true;
   onOpenModal!: (() => void);
+  onAddRange!: (() => void);
 }
 
-function TopHeader({onOpenModal}: TopHeaderProps) {
+function TopHeader({addRangeEnabled, onOpenModal, onAddRange}: TopHeaderProps) {
 
     return (<Header className='display-flex header'>
       <Button onClick={onOpenModal}>
         Get Manifest
+      </Button>
+      <Button onClick={onAddRange} disabled={!addRangeEnabled}>
+        +
       </Button>
     </Header>);
 }
