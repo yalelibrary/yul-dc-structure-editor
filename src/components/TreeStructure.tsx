a01b40d546850b9d444fb62f3201d85323a5df82
K8Sewell <kait@notch8.com>
2022-08-17 10:35:33 -0700

    Adds drag and drop ability

--- src/components/TreeStructure.tsx
+++ src/components/TreeStructure.tsx
@@ -1,5 +1,5 @@
 import { Tree } from 'antd';
-import { DataNode } from 'antd/lib/tree';
+import { DataNode, TreeProps } from 'antd/lib/tree';
 import React, { useState } from 'react';
 import { ManifestCanvasInfo, ManifestStructureInfo } from '../utils/IIIFUtils';
 import EditableText from './EditableText';
@@ -128,8 +128,85 @@ function TreeStructure({ structureInfo, selectedIds, expandedIds, canvasInfo, on
     })
   }
 
+  const onDragEnter: TreeProps['onDragEnter'] = (info: any) => {
+    console.log(info);
+    // expandedKeys 需要受控时设置
+    // setExpandedKeys(info.expandedKeys)
+  };
+
+  const onDrop: TreeProps['onDrop'] = (info: any) => {
+    console.log(info);
+    const dropKey = info.node.key;
+    const dragKey = info.dragNode.key;
+    const dropPos = info.node.pos.split('-');
+    const dropPosition = info.dropPosition - Number(dropPos[dropPos.length - 1]);
+
+    const loop = (
+      data: ManifestStructureInfo[],
+      id: string,
+      callback: (node: ManifestStructureInfo, i: number, data: ManifestStructureInfo[]) => void,
+    ) => {
+      for (let i = 0; i < data.length; i++) {
+        if (data[i].id === id) {
+          return callback(data[i], i, data);
+        }
+        if (data[i].items) {
+          loop(data[i].items!, id, callback);
+        }
+      }
+    };
+    const data = [...structureInfo];
+
+    // Find dragObject
+    let dragObj: ManifestStructureInfo;
+    loop(data, dragKey, (item, index, arr) => {
+      arr.splice(index, 1);
+      dragObj = item;
+    });
+
+    if (!info.dropToGap) {
+      // Drop on the content
+      loop(data, dropKey, item => {
+        item.items = item.items || [];
+        // where to insert 示例添加到头部，可以是随意位置
+        item.items.unshift(dragObj);
+      });
+    } else if (
+      ((info.node as any).props.items || []).length > 0 && // Has items
+      (info.node as any).props.expanded && // Is expanded
+      dropPosition === 1 // On the bottom gap
+    ) {
+      loop(data, dropKey, item => {
+        item.items = item.items || [];
+        // where to insert 示例添加到头部，可以是随意位置
+        item.items.unshift(dragObj);
+        // in previous version, we use item.children.push(dragObj) to insert the
+        // item to the tail of the children
+      });
+    } else {
+      let ar: ManifestStructureInfo[] = [];
+      let i: number;
+      loop(data, dropKey, (_item, index, arr) => {
+        ar = arr;
+        i = index;
+      });
+      if (dropPosition === -1) {
+        ar.splice(i!, 0, dragObj!);
+      } else {
+        ar.splice(i! + 1, 0, dragObj!);
+      }
+    }
+    onChangeStructureInfo(data);
+  };
+
+
   return (
     <Tree
+      className="draggable-tree"
+      draggable
+      blockNode
+      onDragEnter={onDragEnter}
+      onDrop={onDrop}
       multiple={true}
       treeData={mapStructureToDataNodes(structureInfo)}
       selectedKeys={selectedIds}
