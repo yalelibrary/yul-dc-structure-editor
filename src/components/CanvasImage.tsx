47c20b724b1cd2ea0af76eda363d76d2c55ffb09
Martin Lovell <martin.lovell@yale.edu>
2022-09-02 13:03:39 -0400

    Add zooming

--- src/components/CanvasImage.tsx
+++ src/components/CanvasImage.tsx
@@ -7,9 +7,10 @@ class CanvasImageProps {
     info!: ManifestCanvasInfo;
     maxWidthHeight: number = 200;
     onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+    onDoubleClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
 }
 
-function CanvasImage({ selected, info, maxWidthHeight, onClick }: CanvasImageProps) {
+function CanvasImage({ selected, info, maxWidthHeight, onClick, onDoubleClick }: CanvasImageProps) {
     const [loaded, setLoaded] = useState(false);
     const { ref, inView } = useInView({
         threshold: 0,
@@ -24,7 +25,7 @@ function CanvasImage({ selected, info, maxWidthHeight, onClick }: CanvasImagePro
     }
     return <>
         <div ref={divRef} className={'item' + (selected ? " selected" : "")} >
-            <div key={info.imageId} onClick={onClick} ref={ref} >
+            <div key={info.imageId} onClick={onClick} ref={ref} onDoubleClick={onDoubleClick} >
                 {(inView || loaded) && <img src={info.thumbnail.replace("/!200,200/", `/!${maxWidthHeight},${maxWidthHeight}/`)} loading="lazy" alt={info.label + " " + info.oid} onLoad={handleImageLoaded} ref={imageRef} />}
                 <br />
                 <div className='item-info'>
---
+++ src/components/OpenSeadragonViewer.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect, useLayoutEffect, useRef } from 'react';
+import OpenSeaDragon from "openseadragon";
+
+class OpenSeadragonViewerProps {
+    imageUrl!: string;
+    elementId?: string;
+}
+
+function OpenSeadragonViewer({ imageUrl, elementId }: OpenSeadragonViewerProps) {
+    const viewerRef = useRef<OpenSeaDragon.Viewer | null>(null);
+
+    if (!elementId) {
+        elementId = "OpenSeadragon" + Math.round(Math.random() * 100000);
+    }
+
+    const initializeViewer = () => {
+        viewerRef.current && viewerRef.current.destroy();
+        try {
+            viewerRef.current =
+                OpenSeaDragon({
+                    id: elementId,
+                    tileSources: [imageUrl],
+                    prefixUrl: 'https://cdn.jsdelivr.net/gh/Benomrans/openseadragon-icons@main/images/',
+                    animationTime: 0.5,
+                    autoHideControls: false,
+                    blendTime: 0.1,
+                    showNavigationControl: true,
+                    showRotationControl: true,
+                    showFlipControl: true,
+                    maxZoomPixelRatio: 2,
+                    visibilityRatio: 1,
+                    zoomPerScroll: 2,
+                });
+
+        }
+        catch (e) {
+            viewerRef.current = null;
+            console.error(e);
+        }
+    };
+
+    useEffect(() => {
+        if (imageUrl && viewerRef.current) {
+            viewerRef.current.open(imageUrl);
+        }
+    }, [imageUrl]);
+
+    useLayoutEffect(() => {
+        setTimeout(initializeViewer, 100);
+        return () => {
+            viewerRef.current && viewerRef.current.destroy();
+            viewerRef.current = null;
+        };
+    }, []);
+
+    return (
+        <div
+            id={elementId}
+            style={{
+                height: "calc(100vh - 280px)",
+                width: "100%"
+            }}
+        >
+        </div>
+    );
+};
+
+export default OpenSeadragonViewer;
